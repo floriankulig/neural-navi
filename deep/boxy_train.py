83b0b8586469f872272c78468b1238e9e8ce0fc7
floriankulig <florian.kulig@web.de>
2025-05-23 13:33:30 +0200

    refactor: adjust GPU usage and training parameters for improved performance

--- deep/boxy_train.py
+++ deep/boxy_train.py
@@ -7,8 +7,8 @@ YOLOv12n Training Script for Boxy Dataset
 from ultralytics import YOLO
 
 def main():
-    num_gpus = 4
-    batch_size = 64 * num_gpus  
+    num_gpus = 3
+    batch_size = 48 * num_gpus  
     devices = [-1] * num_gpus
 
     # Load pre-trained model
@@ -16,57 +16,38 @@ def main():
     
     # Training hyperparameters
     hyperparameters = {
-        'data': 'data/dataset_nc3_skip.yaml',
-        'epochs': 42,
-        'close_mosaic': 8,
-        'patience': 10,
+        'data': 'data/dataset_nc1_hard.yaml',
+        'epochs': 100,
+        'close_mosaic': 10,
+        'patience': 15,
         'batch': batch_size,
         'imgsz': 704,
-        'multi_scale': True,
-        'optimizer': 'AdamW',
-        'warmup_epochs': 5,
-        'lr0': 0.0075,
-        'cos_lr': True,
-        'degrees': 4,
-        'mixup': 0.25,
-        'cutmix': 0.15,
-        'save_period': 5,
-        'workers': 8,
-        # 'cls': 1,
+        'multi_scale': False,
+        # 'optimizer': 'AdamW',
+        # 'warmup_epochs': 4,
+        # 'lr0': 0.0075,
+        # 'cos_lr': True,
+
+        # Augmentations angepasst
+        'degrees': 6,    # Weniger, da Kamera fest montiert
+        'perspective': 0.00025,  # Minimal
+        
+        'mixup': 0.0,  # Aus - problematisch für kleine Objekte
+        'cutmix': 0.05,   # Aus - problematisch für kleine Objekte
+        'copy_paste': 0.1,  # Kann helfen!
+        'close_mosaic': 10,
+
+        'save_period':  5, 
+        'workers': 12,
+        'cls': 0.575,
         'device': devices,
         'dropout': 0.15,
         'pretrained': True,
         'plots': True,
         'val': True,
-        'single_cls': False,
-        'name': 'yolo12n3_skip',
+        'project': 'runs/final',
+        'name': 'yolo12n1_auto',
     }
-    # model = YOLO("best.pt")
-    # hyperparameters = {
-    #     'data': 'data/dataset_nc3.yaml',
-    #     'epochs': 15,
-    #     'patience': 10,
-    #     'batch': batch_size,
-    #     'imgsz': 704,
-    #     'multi_scale': True,
-    #     'optimizer': 'AdamW',
-    #     'warmup_epochs': 2,
-    #     'lr0': 0.0025,
-    #     'cos_lr': True,
-    #     'degrees': 4,
-    #     'mixup': 0.25,
-    #     'cutmix': 0.15,
-    #     'close_mosaic': 10,
-    #     'save_period': 2,
-    #     'workers': 8,
-    #     'classes': [1],
-    #     'device': devices,
-    #     'dropout': 0.15,
-    #     'pretrained': True,
-    #     'plots': True,
-    #     'val': True,
-    #     'name': 'yolo12n3_tunefront',
-    # }
     
     # Start training
     model.train(**hyperparameters)
